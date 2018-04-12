0b6ff1eb61d02868122b86d7eca7b6e6e8efb64a
drewdearing <social@drewdearing.com>
2018-04-12 00:31:58 -0500

    Update to use JQuery

--- content.js
+++ content.js
@@ -1,34 +1,54 @@
 class Step {
-	constructor(icon = "fa-play-circle", body_text = "Card"){
+	constructor(icon = "fa-play-circle"){
 		this.hidden = true;
-		this.element = document.createElement("div");
-		this.element.className = "hide";
-		this.icon = icon;
-		this.body = body_text;
-		this.element.innerHTML = "<div class=\"step-contents\">"
-		+"<i class=\"fa "+ this.icon +"\"></i>"
-		+"<div class=\"step-body\">"+ this.body +"</div>"
-		+"<div class=\"step-footer\">"
-			+"<div class=\"progress progress-success\">"
-				+"<div class=\"progress-bar progress-bar-success\" id=\"tournament-progress\" style=\"width: 1%\">"
-				+"</div>"
-			+"</div>"
-		+"</div>"
-		+"</div>";
-		var next_steps = document.getElementsByClassName("next-steps")[0];
-		var clear_steps = next_steps.getElementsByClassName("clear-steps")[0];
-		next_steps.insertBefore(this.element, clear_steps)
+		this.icon_name = icon;
+		this.element = $("<div>", {"class": "hide"});
+		this.step_contents = $("<div>", {"class": "step-contents"});
+		this.icon = $("<i>", {"class": "fa " + this.icon_name});
+		this.step_body = $("<div>", {"class": "step-body"});
+		this.step_footer = $("<div>", {"class": "step-footer"});
+		this.element.append(this.step_contents);
+		this.step_contents.append(this.icon);
+		this.step_contents.append(this.step_body);
+		this.step_contents.append(this.step_footer);
+
+		var clear_steps = $(".next-steps").first().find("div.clear-steps");
+		clear_steps.before(this.element);
 	}
 
 	hide(){
-		this.element.className = "hide";
+		if(!this.hidden){
+			this.element.addClass('hide').removeClass('step');
+			this.hidden = true;
+		}
 	}
 
 	show(){
-		this.element.className = "step";
+		if(this.hidden){
+			this.element.addClass('step').removeClass('hide');
+			this.hidden = false;
+		}
 	}
 
+	updateBody(body){
+		this.step_body.replaceWith(body);
+	}
+
+	updateIcon(new_icon){
+		this.icon.addClass(new_icon).removeClass(this.icon_name);
+		this.icon_name = new_icon;
+	}
+
+	updateFooter(footer){
+		this.step_footer.replaceWith(footer);
+	}
 }
 
-var stream_step = new Step();
-stream_step.show();
+$(function(){
+	var stream_step = new Step();
+	stream_step.show();
+
+	var open_matches = $('.match.-open');
+
+	console.log("Number of open matches: "+open_matches.length);
+});
